286f01dc99b3d082cc0309260f68eda8d20be254
Harshit-K01 <harshitk@naehas.com>
2023-04-04 14:53:02 +0530

    some updates in fetching duration of flights logic

--- src/main/java/com/MakeMyTrip/springboot/mmt_project/dao/FlightDetailsRepository.java
+++ src/main/java/com/MakeMyTrip/springboot/mmt_project/dao/FlightDetailsRepository.java
@@ -11,6 +11,6 @@ import java.util.List;
 
 public interface FlightDetailsRepository extends JpaRepository<FlightDetail, Integer> {
 
-    @Query("Select f.flightNumber, f.airline, f.arriveDay,f.departDay,f.departTime,f.arriveTime,f.source,f.destination, fd.classType, fd.fare from FlightDetail f, FareDetail fd where f.flightNumber=fd.flightNumber and f.source=?1 and f.destination=?2 and f.departDay=?3 and fd.classType=?4 ")
+    @Query("Select f.flightNumber, f.airline, f.arriveDay,f.departDay,f.departTime,f.arriveTime,f.source,f.destination, fd.classType, fd.fare,f.duration from FlightDetail f, FareDetail fd where f.flightNumber=fd.flightNumber and f.source=?1 and f.destination=?2 and f.departDay=?3 and fd.classType=?4 ")
     List<Object> findBySourceAndDestinationAndDepartDayAndClassType(@Param("source") String source, @Param("destination") String destination, @Param("departDay") LocalDate departDay, @Param("classType") String classType);
 }
